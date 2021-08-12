8a19c55965217e10b53a3e1379ebb01afa5d822c
Kara Raithe <55517663+kraithe@users.noreply.github.com>
2021-08-12 17:28:07 -0400

    Configure webpack, babel for React and Sass loaders

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client');
+const DIST_DIR = path.join(__dirname, '/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  devServer: {
+    contentBase: DIST_DIR
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  }
+};
