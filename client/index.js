3b548e8d9a7856b0b1e72db78b998d3a6391cee5
Kara Raithe <55517663+kraithe@users.noreply.github.com>
2021-08-12 17:42:11 -0400

    Create central index file for React rendering

---
+++ client/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './components/style.scss';
+
+ReactDOM.render(<App />, document.getElementById('journal-app'));
