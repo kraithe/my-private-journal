0fa9deb45339c2a3b479887ace39e15af7c75509
Kara Raithe <55517663+kraithe@users.noreply.github.com>
2021-08-12 18:03:09 -0400

    Create primary entries display with read, edit, delete paths

---
+++ client/components/EntryList.js
@@ -0,0 +1,46 @@
+import React, { Fragment, useState } from 'react';
+import EditEntry from './EditEntry';
+import axios from 'axios';
+
+const EntryList = (props) => {
+  const deleteEntry = async (deleted) => {
+    try {
+      const deletion = await axios.delete(`http://localhost:3950/entry/${deleted}`);
+      window.location = '/'
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <table className="entrylist-content">
+        <thead>
+          <tr>
+            <th>Journal date</th>
+            <th>Edit entry</th>
+            <th>Remove entry</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.entries.map(entry => (
+            <tr key={entry.entry_id}>
+              <td>{entry.entry_modified || entry.entry_created}</td>
+              <td>{entry.entry_text}</td>
+              <td>
+                <EditEntry journalData={entry} />
+              </td>
+              <td>
+                <button onClick={() => deleteEntry(entry.entry_id)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </React.Fragment>
+  );
+};
+
+export default EntryList;
