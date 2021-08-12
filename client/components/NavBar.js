de037727e8cef2a4b9787b1c10fd68aefaa9ffa7
Kara Raithe <55517663+kraithe@users.noreply.github.com>
2021-08-12 18:00:02 -0400

    Create NavBar with full dataset text search bar

---
+++ client/components/NavBar.js
@@ -0,0 +1,40 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import CreateEntry from './AddEntry.js';
+
+const NavBar = (props) => {
+  let [searchTerm, setSearchTerm] = useState('');
+
+  const fullTextSearch = function(e) {
+    e.preventDefault();
+    let filtered = [];
+    for (let i = 0; i < props.entries.length; i++) {
+      if (props.entries[i].entry_text.includes(searchTerm)) {
+        filtered.push(props.entries[i]);
+      }
+    }
+    props.updateEntries(filtered);
+  }
+
+  return (
+    <React.Fragment>
+      <div className="navbar-content">
+        <CreateEntry />
+        <form>
+          Search your journal by topic
+          <input
+            type="text"
+            value={searchTerm}
+            placeholder="e.g. school, anniversary, travel"
+            onChange={(e) => setSearchTerm(e.target.value)}
+            >
+          </input>
+        </form>
+        <button onClick={(e) => fullTextSearch(e)}>
+          Search
+        </button>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default NavBar;
